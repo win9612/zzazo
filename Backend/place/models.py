17380ee5ce2533d1fff61d7996d594038df41ba2
박성배 <98007970+SeongbaePark1105@users.noreply.github.com>
2022-09-15 13:29:10 +0900

    develop: feat/BE/sung commit

--- Backend/place/models.py
+++ Backend/place/models.py
@@ -6,7 +6,7 @@ class Place(models.Model):
     address = models.CharField(max_length=100)
     longitude = models.CharField(max_length=100)
     latitude = models.CharField(max_length=100)
-    Type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
     regist = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
 
---
+++ Backend/place/serializers/place.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from rest_framework import serializers
+from ..models import Place
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
+        fields = ('id', 'name', 'address', 'longitude', 'latitude', 'type')
